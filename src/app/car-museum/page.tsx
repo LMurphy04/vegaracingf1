f4bd1d3315876f5693743797ea4e75632ea22ad2
“Liam <2759113M@student.gla.ac.uk>
2024-07-27 02:08:40 +0100

    Holiday changes

--- src/app/car-museum/page.tsx
+++ src/app/car-museum/page.tsx
@@ -1,20 +1,280 @@
 "use client";
 import Title from "../title";
+import { Canvas, useFrame } from "@react-three/fiber";
+import {
+  OrbitControls,
+  useGLTF,
+  Stage,
+  PresentationControls,
+  Center,
+  CameraControls,
+} from "@react-three/drei";
 import { useRef, useState } from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, useGLTF } from "@react-three/drei";
-import { Mesh } from "three";
+
+function BMW() {
+  const { nodes, materials } = useGLTF("models/bmw.glb");
+  return (
+    <group dispose={null}>
+      <group position={[-286.371, 0, 164.751]} rotation={[Math.PI / 2, 0, 0]}>
+        <mesh
+          geometry={nodes.Object_4.geometry}
+          material={materials.Meshesblack161Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_5.geometry}
+          material={materials.Meshespiggrill1Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_6.geometry}
+          material={materials.Mesheszx1Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_7.geometry}
+          material={materials.Mesheszx1Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_8.geometry}
+          material={materials.Mesheswhite41Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_9.geometry}
+          material={materials.Meshesraidiator1Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_10.geometry}
+          material={materials.Meshesinterior71Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_11.geometry}
+          material={materials.Meshespillars1Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_12.geometry}
+          material={materials.Meshespillars1Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_13.geometry}
+          material={materials.Meshesheadlight51Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_14.geometry}
+          material={materials.Mesheslogo11Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_15.geometry}
+          material={materials.Meshesdash11Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_16.geometry}
+          material={materials.Mesheschrome51Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_17.geometry}
+          material={materials.Meshesredlight1Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_18.geometry}
+          material={materials.Meshesinterior231Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_19.geometry}
+          material={materials.Meshesinterior181Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_20.geometry}
+          material={materials.Mesheslight51Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_21.geometry}
+          material={materials.Meshesengine51Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_22.geometry}
+          material={materials.Meshesled11Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_23.geometry}
+          material={materials.Meshesbody151Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_24.geometry}
+          material={materials.Meshesblack151Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_25.geometry}
+          material={materials.Mesheswindows1Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_26.geometry}
+          material={materials.Mesheslogo31Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_27.geometry}
+          material={materials.Meshesled111Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_28.geometry}
+          material={materials.Mesheslivery1Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_29.geometry}
+          material={materials.Meshestail71Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_30.geometry}
+          material={materials.Meshesff41Mtl}
+        />
+      </group>
+      <group position={[-286.371, 0, 164.751]} rotation={[Math.PI / 2, 0, 0]}>
+        <mesh
+          geometry={nodes.Object_32.geometry}
+          material={materials.Caliper1Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_33.geometry}
+          material={materials.Meshesm8rim1Mtl}
+        />
+        <mesh
+          geometry={nodes.Object_34.geometry}
+          material={materials.Meshesm8rim0011Mtl}
+        />
+      </group>
+    </group>
+  );
+}
 
 export default function Home() {
-  const ref = useRef<Mesh>(null!);
+  const [rotate, setRotate] = useState(true);
+  const [x, setX] = useState(0);
+
   return (
     <>
       <Title title="Car Museum" />
-      <Canvas className="w-full h-[30rem] bg-black shadow-[inset_0px_0px_5px_5px_rgb(255,255,255,1.00)]">
-        <mesh ref={ref}>
-          <circleGeometry attach="geometry" args={[2, 1000, 2]} />
-        </mesh>
-      </Canvas>
+      <div className="relative mb-5">
+        <Canvas
+          dpr={[1, 2]}
+          className="w-full h-[30rem] bg-black touch-none"
+          camera={{ fov: 45, position: [30, 10, 10] }}
+          onClick={() => setRotate(false)}
+        >
+          <OrbitControls
+            maxPolarAngle={Math.PI / 2}
+            maxDistance={30}
+            autoRotate={rotate}
+            makeDefault
+          />
+          <ambientLight intensity={0.2} />
+          <spotLight
+            position={[20, 30, 20]}
+            angle={0.4}
+            penumbra={1}
+            decay={0}
+            intensity={Math.PI}
+          />
+          <spotLight
+            position={[-20, 30, -20]}
+            angle={0.4}
+            penumbra={1}
+            decay={0}
+            intensity={Math.PI}
+          />
+          <spotLight
+            position={[20, 30, -20]}
+            angle={0.4}
+            penumbra={1}
+            decay={0}
+            intensity={Math.PI}
+          />
+          <spotLight
+            position={[-20, 30, 20]}
+            angle={0.4}
+            penumbra={1}
+            decay={0}
+            intensity={Math.PI}
+          />
+          {/* <directionalLight
+            color={"white"}
+            position={[10, 10, 10]}
+            intensity={5}
+          />
+          <directionalLight
+            color={"white"}
+            position={[-10, 10, -10]}
+            intensity={5}
+          /> */}
+          <Center position={[0 - x, -100, 0]} top>
+            <Center bottom>
+              <mesh rotation={[0, 0, 0]} receiveShadow>
+                <cylinderGeometry args={[12, 12, 100, 100]} />
+                <meshStandardMaterial color="red" />
+              </mesh>
+            </Center>
+            <Center top>
+              <BMW />
+            </Center>
+          </Center>
+          <Center position={[50 - x, -100, 0]} top>
+            <Center bottom>
+              <mesh rotation={[0, 0, 0]} receiveShadow>
+                <cylinderGeometry args={[12, 12, 100, 100]} />
+                <meshStandardMaterial color="red" />
+              </mesh>
+            </Center>
+            <Center top>
+              <BMW />
+            </Center>
+          </Center>
+          <Center position={[100 - x, -100, 0]} top>
+            <Center bottom>
+              <mesh rotation={[0, 0, 0]} receiveShadow>
+                <cylinderGeometry args={[12, 12, 100, 100]} />
+                <meshStandardMaterial color="red" />
+              </mesh>
+            </Center>
+            <Center top>
+              <BMW />
+            </Center>
+          </Center>
+          <Center position={[150 - x, -100, 0]} top>
+            <Center bottom>
+              <mesh rotation={[0, 0, 0]} receiveShadow>
+                <cylinderGeometry args={[12, 12, 100, 100]} />
+                <meshStandardMaterial color="red" />
+              </mesh>
+            </Center>
+            <Center top>
+              <BMW />
+            </Center>
+          </Center>
+          <Center position={[200 - x, -100, 0]} top>
+            <Center bottom>
+              <mesh rotation={[0, 0, 0]} receiveShadow>
+                <cylinderGeometry args={[12, 12, 100, 100]} />
+                <meshStandardMaterial color="red" />
+              </mesh>
+            </Center>
+            <Center top>
+              <BMW />
+            </Center>
+          </Center>
+        </Canvas>
+        <div className="absolute top-0 start-0">
+          <button
+            onClick={() => setRotate(!rotate)}
+            className="bg-white p-2 m-2"
+          >
+            Auto Rotate
+          </button>
+          <button onClick={() => setX(x - 50)} className="bg-white p-2 m-2">
+            {"<"}
+          </button>
+          <button onClick={() => setX(x + 50)} className="bg-white p-2 m-2">
+            {">"}
+          </button>
+        </div>
+      </div>
     </>
   );
 }
+
+useGLTF.preload("/models/bmw.glb");
--- src/app/meet-the-team/page.tsx
+++ src/app/meet-the-team/page.tsx
@@ -11,7 +11,7 @@ export default function MeetTheTeam() {
     <>
       <Title title={"Meet the Team"} />
       <div className="flex flex-wrap">
-        <ImageCollection tab={setActiveTab} />
+        <ImageCollection tab={setActiveTab} active={activeTab} />
       </div>
       <Tabs
         value={activeTab}
@@ -37,11 +37,24 @@ export default function MeetTheTeam() {
         {membersInfo.map((member, index) => {
           return (
             <Tabs.Panel key={index} value={member.name}>
-              <div className="pb-3 pr-3 border-b-[1px] border-black">
-                <p className="text-xl font-bold">{member.name}</p>
-                <p className="text-md font-light">{member.role}</p>
+              <div className="w-full flex flex-col md:flex-row gap-5">
+                <div>
+                  <div className="pb-3 pr-3 border-b-[1px] border-black">
+                    <p className="text-xl font-bold">{member.name}</p>
+                    <p className="text-md font-light">{member.role}</p>
+                  </div>
+                  <p className="mt-3 whitespace-pre text-wrap">{member.bio}</p>
+                </div>
+                <div className="w-full md:w-[400px] md:min-w-[400px] self-start bg-white shadow-xl rounded-sm hover:scale-[1.02] transition-all">
+                  <p className="text-white p-3 bg-vega-blue">
+                    {member.name}&apos;s Factsheet
+                  </p>
+                  <p className="p-3">{`> Favourite Grand Prix:`}</p>
+                  <p className="p-3">{`> Favourite Grand Prix:`}</p>
+                  <p className="p-3">{`> Favourite Grand Prix:`}</p>
+                  <p className="p-3">{`> Favourite Grand Prix fwnkfa fdJKFHDS UFH fSUHIK FUSHIDSF UIFDHS:`}</p>
+                </div>
               </div>
-              <p className="mt-3 whitespace-pre text-wrap">{member.bio}</p>
             </Tabs.Panel>
           );
         })}
@@ -50,7 +63,13 @@ export default function MeetTheTeam() {
   );
 }
 
-function ImageCollection({ tab }: { tab: Function }) {
+function ImageCollection({
+  tab,
+  active,
+}: {
+  tab: Function;
+  active: string | null;
+}) {
   return membersInfo.map((memberInfo, index) => (
     <MemberImage
       key={index}
@@ -58,6 +77,7 @@ function ImageCollection({ tab }: { tab: Function }) {
       role={memberInfo.role}
       image={memberInfo.image}
       tab={tab}
+      active={active}
     />
   ));
 }
@@ -67,22 +87,29 @@ function MemberImage({
   image,
   tab,
   role,
+  active,
 }: {
   name: string;
   image: string;
   tab: Function;
   role: string;
+  active: string | null;
 }) {
+  const parentStyle =
+    active == name
+      ? `${image} group bg-center bg-no-repeat bg-cover basis-1/2 sm:flex-1 flex border-2 border-[#F5F5F5] h-[300px] sm:flex-[2] duration-[1000ms] sm:border-b-vega-blue sm:border-b-8 transition-all`
+      : `${image} group bg-center bg-no-repeat bg-cover basis-1/2 sm:flex-1 flex border-2 border-[#F5F5F5] h-[300px] duration-[1000ms] transition-all`;
+
+  const roleStyle =
+    active == name
+      ? "transition-all duration-[1000ms] opacity-100 text-base text-white font-bold text-wrap"
+      : "transition-all duration-[1000ms] sm:opacity-0 sm:text-[0px] text-white font-bold text-wrap";
+
   return (
-    <div
-      className={`${image} group bg-center bg-no-repeat bg-cover basis-1/2 sm:flex-1 flex border-2 border-[#F5F5F5] h-[300px] sm:hover:flex-[2] duration-[1000ms] sm:hover:border-b-vega-blue sm:hover:border-b-8 transition-all`}
-      onMouseOver={() => tab(name)}
-    >
+    <div className={parentStyle} onMouseOver={() => tab(name)}>
       <div className="mt-auto p-3">
         <p className="text-white font-bold text-2xl">{name}</p>
-        <p className="transition-all duration-[1000ms] sm:opacity-0 sm:text-[0px] group-hover:opacity-100 group-hover:text-base text-white font-bold text-wrap">
-          {role}
-        </p>
+        <p className={roleStyle}>{role}</p>
       </div>
     </div>
   );
@@ -93,36 +120,36 @@ const membersInfo = [
     name: "Sally",
     role: "Project Manager",
     image: "bg-[url('/exampleMugshot.jpg')]",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur justo orci, dignissim nec justo ac, cursus finibus turpis. Duis tristique, lacus sed venenatis luctus, neque turpis aliquet tellus, id dictum odio orci id ante. Sed imperdiet a urna sit amet congue. Etiam sed turpis posuere, pulvinar est eu, egestas leo. Sed id ante porttitor, blandit odio a, venenatis nisl. Etiam tortor turpis, laoreet vitae neque in, fringilla maximus nibh. Cras imperdiet elit ipsum, ac euismod nibh consectetur a. In feugiat nibh dolor, eget malesuada nibh fringilla nec. Nullam id laoreet nibh. Vestibulum feugiat, sapien eget viverra fringilla, mi tortor vehicula nulla, viverra vehicula nisi sem semper enim.",
+    bio: `As the Project Manager of Vega Racing, Sally uses her orginisational skills to make sure we're all on track for success. In her spare time, Sally enjoys drumming, reading, and baking. Her favourite book genre is science fiction.`,
   },
   {
     name: "Evie",
     role: "Graphic Designer",
     image: "bg-[url('/exampleMugshot.jpg')]",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur justo orci, dignissim nec justo ac, cursus finibus turpis. Duis tristique, lacus sed venenatis luctus, neque turpis aliquet tellus, id dictum odio orci id ante. Sed imperdiet a urna sit amet congue. Etiam sed turpis posuere, pulvinar est eu, egestas leo. Sed id ante porttitor, blandit odio a, venenatis nisl. Etiam tortor turpis, laoreet vitae neque in, fringilla maximus nibh. Cras imperdiet elit ipsum, ac euismod nibh consectetur a. In feugiat nibh dolor, eget malesuada nibh fringilla nec. Nullam id laoreet nibh. Vestibulum feugiat, sapien eget viverra fringilla, mi tortor vehicula nulla, viverra vehicula nisi sem semper enim.",
+    bio: `Evie is Vega Racing's Graphic Designer. She designs the team kit, folio layout, and the overall branding and identity of the team. Evie enjoys playing netball in which she plays goal attack, and is currently training to run a half marathon.`,
   },
   {
     name: "Joe",
     role: "Design Engineer",
     image: "bg-[url('/exampleMugshot.jpg')]",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur justo orci, dignissim nec justo ac, cursus finibus turpis. Duis tristique, lacus sed venenatis luctus, neque turpis aliquet tellus, id dictum odio orci id ante. Sed imperdiet a urna sit amet congue. Etiam sed turpis posuere, pulvinar est eu, egestas leo. Sed id ante porttitor, blandit odio a, venenatis nisl. Etiam tortor turpis, laoreet vitae neque in, fringilla maximus nibh. Cras imperdiet elit ipsum, ac euismod nibh consectetur a. In feugiat nibh dolor, eget malesuada nibh fringilla nec. Nullam id laoreet nibh. Vestibulum feugiat, sapien eget viverra fringilla, mi tortor vehicula nulla, viverra vehicula nisi sem semper enim.",
+    bio: `Our Design Engineer, Joe, is responsible for designing the miniature car that we race at competitions. While considering the strict rules and regulations set by F1 in Schools, his aim is to make our car the fastest on the track. In his spare time, Joe enjoys playing video games, running, and watching his favourite F1 team, McLaren.`,
   },
   {
     name: "Calum",
     role: "Manufacturing Engineer",
     image: "bg-[url('/exampleMugshot.jpg')]",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur justo orci, dignissim nec justo ac, cursus finibus turpis. Duis tristique, lacus sed venenatis luctus, neque turpis aliquet tellus, id dictum odio orci id ante. Sed imperdiet a urna sit amet congue. Etiam sed turpis posuere, pulvinar est eu, egestas leo. Sed id ante porttitor, blandit odio a, venenatis nisl. Etiam tortor turpis, laoreet vitae neque in, fringilla maximus nibh. Cras imperdiet elit ipsum, ac euismod nibh consectetur a. In feugiat nibh dolor, eget malesuada nibh fringilla nec. Nullam id laoreet nibh. Vestibulum feugiat, sapien eget viverra fringilla, mi tortor vehicula nulla, viverra vehicula nisi sem semper enim.",
+    bio: `Calum is our Manufacturing Engineer and Team Driver. He manages the manufacturing process and assembly of the car. He also researches manufacturing methods and materials to make sure our car is fast and compliant. He physically tests the car to ensure it is suitable for competition. Being the Team Driver means that at competitions, Calum is responsible for firing the car down the track with his quick reaction times. In his spare time, Calum likes to play video games and read. He also plays left wing at football.`,
   },
   {
     name: "Kirsty",
     role: "Sponsorship Manager",
     image: "bg-[url('/exampleMugshot.jpg')]",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur justo orci, dignissim nec justo ac, cursus finibus turpis. Duis tristique, lacus sed venenatis luctus, neque turpis aliquet tellus, id dictum odio orci id ante. Sed imperdiet a urna sit amet congue. Etiam sed turpis posuere, pulvinar est eu, egestas leo. Sed id ante porttitor, blandit odio a, venenatis nisl. Etiam tortor turpis, laoreet vitae neque in, fringilla maximus nibh. Cras imperdiet elit ipsum, ac euismod nibh consectetur a. In feugiat nibh dolor, eget malesuada nibh fringilla nec. Nullam id laoreet nibh. Vestibulum feugiat, sapien eget viverra fringilla, mi tortor vehicula nulla, viverra vehicula nisi sem semper enim.",
+    bio: `As Sponsorship Manager, Kirsty contacts companies to try to gain sponsorship, whether financial or other sponsorship. Outside of F1 in Schools, Kirsty is very busy doing lots of things including running and dancing. She also finds time to play midfield at hockey.`,
   },
   {
     name: "Clara",
     role: "Marketing Manager",
     image: "bg-[url('/exampleMugshot.jpg')]",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur justo orci, dignissim nec justo ac, cursus finibus turpis. Duis tristique, lacus sed venenatis luctus, neque turpis aliquet tellus, id dictum odio orci id ante. Sed imperdiet a urna sit amet congue. Etiam sed turpis posuere, pulvinar est eu, egestas leo. Sed id ante porttitor, blandit odio a, venenatis nisl. Etiam tortor turpis, laoreet vitae neque in, fringilla maximus nibh. Cras imperdiet elit ipsum, ac euismod nibh consectetur a. In feugiat nibh dolor, eget malesuada nibh fringilla nec. Nullam id laoreet nibh. Vestibulum feugiat, sapien eget viverra fringilla, mi tortor vehicula nulla, viverra vehicula nisi sem semper enim.",
+    bio: `Clara is Vega Racing's Marketing Manager. Her job in the team is to organise events to promote and/or fundraise, make marketing materials, and design and build our pit display for the competition. Her hobbies include baking, running, and reading. Her favourite genre to read is dystopian or rom coms.`,
   },
 ];
