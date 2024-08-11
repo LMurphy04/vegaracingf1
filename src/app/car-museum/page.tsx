923362d6b2b9629856fcb179c772b2918c405901
“Liam <2759113M@student.gla.ac.uk>
2024-08-11 20:03:41 +0100

    Car Museum Structure

--- src/app/car-museum/page.tsx
+++ src/app/car-museum/page.tsx
@@ -8,269 +8,161 @@ import {
   PresentationControls,
   Center,
   CameraControls,
+  useFBX,
 } from "@react-three/drei";
+import { Switch, Group } from "@mantine/core";
 import { useRef, useState } from "react";
-
-function BMW() {
-  const { nodes, materials } = useGLTF("models/bmw.glb");
-  return (
-    <group dispose={null}>
-      <group position={[-286.371, 0, 164.751]} rotation={[Math.PI / 2, 0, 0]}>
-        <mesh
-          geometry={nodes.Object_4.geometry}
-          material={materials.Meshesblack161Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_5.geometry}
-          material={materials.Meshespiggrill1Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_6.geometry}
-          material={materials.Mesheszx1Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_7.geometry}
-          material={materials.Mesheszx1Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_8.geometry}
-          material={materials.Mesheswhite41Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_9.geometry}
-          material={materials.Meshesraidiator1Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_10.geometry}
-          material={materials.Meshesinterior71Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_11.geometry}
-          material={materials.Meshespillars1Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_12.geometry}
-          material={materials.Meshespillars1Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_13.geometry}
-          material={materials.Meshesheadlight51Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_14.geometry}
-          material={materials.Mesheslogo11Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_15.geometry}
-          material={materials.Meshesdash11Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_16.geometry}
-          material={materials.Mesheschrome51Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_17.geometry}
-          material={materials.Meshesredlight1Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_18.geometry}
-          material={materials.Meshesinterior231Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_19.geometry}
-          material={materials.Meshesinterior181Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_20.geometry}
-          material={materials.Mesheslight51Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_21.geometry}
-          material={materials.Meshesengine51Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_22.geometry}
-          material={materials.Meshesled11Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_23.geometry}
-          material={materials.Meshesbody151Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_24.geometry}
-          material={materials.Meshesblack151Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_25.geometry}
-          material={materials.Mesheswindows1Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_26.geometry}
-          material={materials.Mesheslogo31Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_27.geometry}
-          material={materials.Meshesled111Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_28.geometry}
-          material={materials.Mesheslivery1Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_29.geometry}
-          material={materials.Meshestail71Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_30.geometry}
-          material={materials.Meshesff41Mtl}
-        />
-      </group>
-      <group position={[-286.371, 0, 164.751]} rotation={[Math.PI / 2, 0, 0]}>
-        <mesh
-          geometry={nodes.Object_32.geometry}
-          material={materials.Caliper1Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_33.geometry}
-          material={materials.Meshesm8rim1Mtl}
-        />
-        <mesh
-          geometry={nodes.Object_34.geometry}
-          material={materials.Meshesm8rim0011Mtl}
-        />
-      </group>
-    </group>
-  );
-}
+import { PI } from "three/examples/jsm/nodes/Nodes.js";
 
 export default function Home() {
   const [rotate, setRotate] = useState(true);
-  const [x, setX] = useState(0);
+  const [car, setCar] = useState(0);
+  const [mode3D, set3D] = useState(true);
+  const [info, setInfo] = useState(false);
+
+  const car1 = useFBX("models/1 - Vega Dev Regionals.fbx");
+  const car2 = useFBX("models/2 - Vega Dev Nationals.fbx");
+  const car3 = useFBX("models/3 - Vega Pro Regionals.fbx");
+  const car4 = useFBX("models/4 - Vega Pro Nationals.fbx");
+
+  const cars = [
+    { name: "Vega Dev Regionals", model: car1 },
+    { name: "Vega Dev Nationals", model: car2 },
+    { name: "Vega Pro Regionals", model: car3 },
+    { name: "Vega Pro Nationals", model: car4 },
+  ];
 
   return (
     <>
       <Title title="Car Museum" />
-      <div className="relative mb-5">
+      <div className="relative mb-5 shadow-xl">
+        <div
+          className={`absolute top-0 w-full h-[30rem] bg-white transition-all duration-1000 ${
+            mode3D ? "opacity-0" : "opacity-100"
+          }`}
+        >
+          {cars[car]["name"]}
+        </div>
         <Canvas
           dpr={[1, 2]}
-          className="w-full h-[30rem] bg-black touch-none"
-          camera={{ fov: 45, position: [30, 10, 10] }}
+          className={`w-full h-[30rem] bg-black touch-none transition-all duration-1000 ${
+            mode3D ? "opacity-100" : "opacity-0"
+          }`}
+          camera={{ fov: 45, position: [-10, 10, 30] }}
           onClick={() => setRotate(false)}
         >
           <OrbitControls
-            maxPolarAngle={Math.PI / 2}
-            maxDistance={30}
+            maxPolarAngle={Math.PI / 2.01}
+            maxDistance={40}
+            minDistance={12}
             autoRotate={rotate}
             makeDefault
+            enablePan={false}
+            enableRotate={mode3D}
           />
-          <ambientLight intensity={0.2} />
+          <ambientLight intensity={0.25} />
           <spotLight
             position={[20, 30, 20]}
             angle={0.4}
             penumbra={1}
             decay={0}
-            intensity={Math.PI}
+            intensity={1}
           />
           <spotLight
             position={[-20, 30, -20]}
             angle={0.4}
             penumbra={1}
             decay={0}
-            intensity={Math.PI}
+            intensity={1}
           />
-          <spotLight
-            position={[20, 30, -20]}
-            angle={0.4}
-            penumbra={1}
-            decay={0}
-            intensity={Math.PI}
-          />
-          <spotLight
-            position={[-20, 30, 20]}
-            angle={0.4}
-            penumbra={1}
-            decay={0}
-            intensity={Math.PI}
-          />
-          {/* <directionalLight
-            color={"white"}
-            position={[10, 10, 10]}
-            intensity={5}
-          />
-          <directionalLight
-            color={"white"}
-            position={[-10, 10, -10]}
-            intensity={5}
-          /> */}
-          <Center position={[0 - x, -100, 0]} top>
-            <Center bottom>
-              <mesh rotation={[0, 0, 0]} receiveShadow>
-                <cylinderGeometry args={[12, 12, 100, 100]} />
-                <meshStandardMaterial color="red" />
-              </mesh>
-            </Center>
-            <Center top>
-              <BMW />
-            </Center>
-          </Center>
-          <Center position={[50 - x, -100, 0]} top>
-            <Center bottom>
-              <mesh rotation={[0, 0, 0]} receiveShadow>
-                <cylinderGeometry args={[12, 12, 100, 100]} />
-                <meshStandardMaterial color="red" />
-              </mesh>
-            </Center>
-            <Center top>
-              <BMW />
-            </Center>
-          </Center>
-          <Center position={[100 - x, -100, 0]} top>
-            <Center bottom>
-              <mesh rotation={[0, 0, 0]} receiveShadow>
-                <cylinderGeometry args={[12, 12, 100, 100]} />
-                <meshStandardMaterial color="red" />
-              </mesh>
-            </Center>
-            <Center top>
-              <BMW />
-            </Center>
-          </Center>
-          <Center position={[150 - x, -100, 0]} top>
-            <Center bottom>
-              <mesh rotation={[0, 0, 0]} receiveShadow>
-                <cylinderGeometry args={[12, 12, 100, 100]} />
-                <meshStandardMaterial color="red" />
-              </mesh>
-            </Center>
-            <Center top>
-              <BMW />
-            </Center>
-          </Center>
-          <Center position={[200 - x, -100, 0]} top>
-            <Center bottom>
-              <mesh rotation={[0, 0, 0]} receiveShadow>
-                <cylinderGeometry args={[12, 12, 100, 100]} />
-                <meshStandardMaterial color="red" />
-              </mesh>
-            </Center>
-            <Center top>
-              <BMW />
-            </Center>
-          </Center>
+          {cars.map((carInfo, index) => {
+            return (
+              <Center key={index} position={[50 * (index - car), -100, 0]} top>
+                <Center bottom>
+                  <mesh rotation={[0, 0, 0]} receiveShadow>
+                    <cylinderGeometry args={[12, 12, 100, 100]} />
+                    <meshStandardMaterial color="gray" />
+                  </mesh>
+                </Center>
+                <Center top>
+                  <mesh rotation={[-Math.PI / 2, 0, -Math.PI / 2]}>
+                    <primitive object={carInfo["model"]} scale={0.1} />
+                  </mesh>
+                </Center>
+              </Center>
+            );
+          })}
         </Canvas>
-        <div className="absolute top-0 start-0">
-          <button
-            onClick={() => setRotate(!rotate)}
-            className="bg-white p-2 m-2"
+        <div
+          className={`absolute top-0 start-0 bg-white h-[30rem] bg-gray-800 flex flex-col transition-all duration-1000  ${
+            mode3D ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          <div className="m-2 text-white">
+            <button
+              className="float-right"
+              onClick={() => {
+                setInfo(!info);
+              }}
+            >
+              {info ? "<<" : ">>"}
+            </button>
+          </div>
+          <div
+            className={`p-2 bg-gray-100 h-full w-[15rem] whitespace-pre text-wrap ${
+              info ? "" : "hidden"
+            }`}
           >
-            Auto Rotate
-          </button>
-          <button onClick={() => setX(x - 50)} className="bg-white p-2 m-2">
-            {"<"}
-          </button>
-          <button onClick={() => setX(x + 50)} className="bg-white p-2 m-2">
-            {">"}
-          </button>
+            {cars[car]["name"]}
+          </div>
+        </div>
+        <div className="absolute bottom-0 start-0 w-full bg-gray-800 p-2">
+          <Group>
+            <Switch
+              size="xl"
+              checked={mode3D}
+              onChange={() => set3D(!mode3D)}
+              color="#fa06a6"
+              onLabel={"3D"}
+              offLabel={"2D"}
+              radius={"md"}
+            />
+            <Switch
+              size="xl"
+              checked={rotate}
+              onChange={() => setRotate(!rotate)}
+              color="#fa06a6"
+              onLabel={"Rotate On"}
+              offLabel={"Rotate Off"}
+              className={`transition-all duration-1000  ${
+                mode3D ? "opacity-100" : "opacity-0"
+              }`}
+              radius={"md"}
+            />
+            <div className="flex flex-row ml-auto rounded-md bg-gray-100 text-sm">
+              <button
+                onClick={() => {
+                  setCar(car - 1);
+                }}
+                className={`p-2 px-4 ${car == 0 ? "text-gray-100" : ""}`}
+                disabled={car == 0}
+              >
+                {"<"}
+              </button>
+              <p className={`p-2 `}>{cars[car]["name"]}</p>
+              <button
+                onClick={() => {
+                  setCar(car + 1);
+                }}
+                className={`p-2 px-4 ${
+                  car == cars.length - 1 ? "text-gray-100" : ""
+                }`}
+                disabled={car == cars.length - 1}
+              >
+                {">"}
+              </button>
+            </div>
+          </Group>
         </div>
       </div>
     </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,35 @@
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <p>‘Working together to reach the stars’</p>
+      <p>
+        Hi, we’re Vega Racing, a professional class F1 in Schools team! This
+        year we’re representing Scotland at the 2024 World Finals in Saudi
+        Arabia, and we’d love to take you along for the ride.
+      </p>
+      <p>
+        What is F1 in Schools? F1 in Schools is an international youth
+        competition with a focus on STEM, where teams of students design, build
+        and race miniature F1 cars. Alongside this, teams must produce
+        portfolios covering their engineering and enterprise work, a pit
+        display, a verbal presentation and fundraise to support their F1 in
+        Schools journey. F1 in Schools offers students the chance to enter the
+        world of STEM, form contacts in F1, and learn practical skills they can
+        apply in later careers. Through both its industry partners and close
+        connection to F1, the competition has offered many a pathway into
+        working in the sport, an opportunity we are eager to take ourselves!
+      </p>
+      <p>
+        About Us We’re a professional class F1 in Schools team formed of six
+        students from Linlithgow Academy, competing in the 2024 World Finals.
+        Previously, we’ve won the Scottish Regional Finals before coming 2 nd in
+        the UK National Finals, allowing us to qualify for Worlds! We’re hugely
+        community oriented, supporting the F1 in Primary Schools competition in
+        our local area and partnering with local businesses to succeed together.
+        We never would have been able to progress to the massive stage of Worlds
+        without our community’s backing, so we hope to continue our crucial link
+        with them into the future.
+      </p>
+    </>
+  );
 }
