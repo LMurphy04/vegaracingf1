7640d22f95eb2149f310ff0b987491a4759c9278
“Liam <2759113M@student.gla.ac.uk>
2024-08-30 15:37:59 +0100

    Blog Page Not Loading New Posts

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,22 +1,26 @@
+"use client";
 import Title from "../title";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
-import BlogPost from "./post";
+import { useEffect, useRef, useState } from "react";
 
-export default async function Blog() {
-  async function fetchBlogPosts() {
-    const querySnapshot = await getDocs(collection(db, "blog"));
-    var data = querySnapshot.docs.map((doc) => ({
-      id: doc.id,
-      heading: doc.get("heading"),
-      body: doc.get("body"),
-      date: doc.get("date"),
-    }));
-    data.sort((a, b) => b.date - a.date);
-    return data;
-  }
+export default function Blog() {
+  const [data, setData] = useState<any>([]);
 
-  var data: {}[] = await fetchBlogPosts();
+  useEffect(() => {
+    async function fetchBlogPosts() {
+      const querySnapshot = await getDocs(collection(db, "blog"));
+      var data = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        heading: doc.get("heading"),
+        body: doc.get("body"),
+        date: doc.get("date"),
+      }));
+      data.sort((a, b) => b.date - a.date);
+      setData(data);
+    }
+    fetchBlogPosts();
+  }, []);
 
   const BlogDisplay = () => {
     if (data === null) {
@@ -52,5 +56,58 @@ export default async function Blog() {
         <BlogDisplay />
       </div>
     </>
+  );
+}
+
+function BlogPost({
+  heading,
+  body,
+  seconds,
+}: {
+  heading: string;
+  body: string;
+  seconds: number;
+}) {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  //toggle visibility once loaded on screen
+  useEffect(() => {
+    const scrollObserver = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) {
+        setIsVisible(true);
+        scrollObserver.unobserve(entry.target);
+      }
+    });
+
+    if (ref.current) {
+      scrollObserver.observe(ref.current);
+    }
+  }, []);
+
+  //converts seconds to dd/mm/yyyy string format
+  function convertSecondsToString(seconds: number) {
+    const jsDate = new Date(seconds * 1000);
+    const dd = String(jsDate.getDate()).padStart(2, "0");
+    const mm = String(jsDate.getMonth() + 1).padStart(2, "0");
+    const yyyy = String(jsDate.getFullYear()).padStart(2, "0");
+    return `${dd}/${mm}/${yyyy}`;
+  }
+
+  return (
+    <div
+      ref={ref}
+      className={`transition-all duration-[2000ms] ease-out border-black shadow-lg shadow-vega-blue delay-500 flex flex-col bg-[#FAFAFA] p-10 ${
+        isVisible ? "" : "translate-x-[-100%] opacity-0"
+      }`}
+    >
+      <p className="text-md font-bold">{heading}</p>
+      <p className="bg-white border-[1px] border-black p-3 rounded-lg my-3 whitespace-pre text-wrap">
+        {body}
+      </p>
+      <p className="flex-1 text-right italic">
+        {convertSecondsToString(seconds)}
+      </p>
+    </div>
   );
 }
